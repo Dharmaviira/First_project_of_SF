fc8bd082916b8a1183ef1cc4c16bcc7bc10b949a
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-25 18:27:32 +0300

    add trening of list

--- game.py
+++ game.py
@@ -20,3 +20,18 @@ while True:
     else:
         print(f"Вы угадали число! Это число = {number}, за {count} попыток")
         break #конец игры выход из цикла
+
+    users = [
+        [15634602, 15647311, 15619304, 15701354], # id
+        ['Hargrave', 'Hill', 'Onio', 'Boni'], # фамилия
+        [619, 608, 502, 699], # кредитный рейтинг
+        ['Female', 'Female', 'Female', 'Female'], # пол
+        [42, 41, 42, 39], # возраст
+        [1, 0, 1, 0] # статус (1 - клиент активен, 0 - ушел)
+         ]
+
+print(sum(users[2][::2]) / len(users[2][::2]))
+
+print(users[1][0] + str(users[0][0]))
+print(users[1][2] + str(users[0][2]))
+
