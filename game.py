13980165dd7a6110355b20f44e702dfb68ccbed7
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-26 15:21:55 +0300

    add slice array

--- game.py
+++ game.py
@@ -54,4 +54,28 @@ import numpy as np
 
 
 # print(np.iinfo(np.uint64))
-print(np.finfo(np.float128))
+# print(np.finfo(np.float128))
+
+# print(np.sctypeDict)
+# arr = np.array([1,5,2,9,10], dtype=np.int8)
+# print(arr.dtype)
+
+# import numpy as np
+# mystery = np.array(
+#     [[-13586,  15203,  28445, -27117,  -1781, -17182, -18049],
+#      [ 25936, -30968,  -1297,  -4593,   6451,  15790,   7181],
+#      [ 13348,  28049,  28655,  -6012,  21762,  25397,   8225],
+#      [ 13240,   7994,  32592,  20149,  13754,  11795,   -564],
+#      [-21725,  -8681,  30305,  22260, -17918,  12578,  29943],
+#      [-16841, -25392, -17278,  11740,   5916,    -47, -32037]],
+#       dtype=np.int16)
+
+# # elem_5_3 = mystery[4,2]
+# # last = mystery[5,6]
+# # line_4 = mystery[3] 
+# # col_2 = mystery[:,5]
+# part = mystery[1:4, 2:5]
+# rev = mystery[:, -1][::-1]
+# # trans = mystery.transpose()
+
+
