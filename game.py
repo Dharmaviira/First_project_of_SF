9d33c2564f9de98146f160b2e8ba932b5ed02590
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-26 12:17:26 +0300

    add sorted cafes right

--- game.py
+++ game.py
@@ -46,6 +46,6 @@ ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foo
            ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
            ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
 
-cafes = OrderedDict(sorted(ratings, key=lambda x: x[1], reverse=True))
+cafes = OrderedDict(sorted(ratings, key=lambda x: ( -x[1], x[0])))
 
 print(cafes)
