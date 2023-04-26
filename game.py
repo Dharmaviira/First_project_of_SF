d5e17489fc6016e2960912750bf27c0c56f0c949
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-26 12:30:43 +0300

    add import numpy

--- game.py
+++ game.py
@@ -40,12 +40,14 @@
 # ordered_client_ages = OrderedDict(sorted(data, key=lambda x: x[0]))
 # print(ordered_client_ages)
 
-from collections import OrderedDict
+# from collections import OrderedDict
 
-ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foods', 4.8),
-           ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
-           ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
+# ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foods', 4.8),
+#            ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
+#            ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
 
-cafes = OrderedDict(sorted(ratings, key=lambda x: ( -x[1], x[0])))
+# cafes = OrderedDict(sorted(ratings, key=lambda x: ( -x[1], x[0])))
 
-print(cafes)
+# print(cafes)
+
+import numpy as np
