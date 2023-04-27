116f7b4f415d71882c1b91b1e4c9a73c5ddecca9
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-27 16:27:53 +0300

    add random number tools

--- game.py
+++ game.py
@@ -103,6 +103,9 @@ np.random.seed(2021)
 
 simple = np.random.rand()
 randoms = np.random.uniform(-150,2021, size = 120)
-table = np.random.randint(1, 100 , size = (3,2)) 
-
-print(table)
+table = np.random.randint(1,101, size = (3,2))
+event = np.arange(2,17,2)
+mix = even.copy()
+np.random.shuffle(mix)
+select = even[1:4]
+triplet = np.random.permutation(select)
