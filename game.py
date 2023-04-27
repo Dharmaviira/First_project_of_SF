88bd68e08a5c7d8366cf6cf8cb9507b00d2d2e66
Dharmaviira <114866197+Dharmaviira@users.noreply.github.com>
2023-04-27 16:50:47 +0300

    add all tools of random numbers

--- game.py
+++ game.py
@@ -98,14 +98,18 @@ import numpy as np
 
 # print(np.random.rand(2, 3, 4, 10, 12, 23))
 
-import numpy as np
-np.random.seed(2021)
+# import numpy as np
+# np.random.seed(2021)
 
-simple = np.random.rand()
-randoms = np.random.uniform(-150,2021, size = 120)
-table = np.random.randint(1,101, size = (3,2))
-event = np.arange(2,17,2)
-mix = even.copy()
-np.random.shuffle(mix)
-select = even[1:4]
-triplet = np.random.permutation(select)
+# simple = np.random.rand()
+# randoms = np.random.uniform(-150,2021, size = 120)
+# table = np.random.randint(1,101, size = (3,2))
+# even = np.arange(2,17,2)
+# mix = even.copy()
+# np.random.shuffle(mix)
+# select = np.random.choice(even, size=3, replace=False)
+# triplet = np.random.permutation(select)
+# print(select)
+# print(triplet)
+
+
